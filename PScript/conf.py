58389e2a8ae864114415a65c0dc6c90a78ff5e44
Alex Winstanley <geometricdomain@gmail.com>
2020-04-16 00:30:05 -0400

    Add files via upload

---
+++ PScript/conf.py
@@ -0,0 +1,4 @@
+URL = "https://discordapp.com/channels/699376645747769487/699376645747769490"
+#URL = "https://discordapp.com/channels/461933238890266634/461933239343513601"
+PASS = "!Lacrosse100"
+USER = "arwds5@gmail.com"
